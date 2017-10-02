f96308d6f6ad01466a289fc07ad5a65dda95861d
mdiamant <diamant.michael@gmail.com>
2017-10-01 23:18:09 -0400

    Checkpoint parser progress

--- zakim-parser/src/main/java/mld/zakim/Parser.java
+++ zakim-parser/src/main/java/mld/zakim/Parser.java
@@ -1,55 +1,96 @@
 package mld.zakim;
 
 public class Parser {
-
-    private static int mapBit(byte[] a, int i, int bitIndex, char c) {
-        int x = ((a[i] ^ c) == 0) ? 1 : 0;
-        return (x << bitIndex);
+    private static long createMask(char c) {
+        long l = 0;
+        l |= c << 8;
+        l |= c << 16;
+        l |= c << 24;
+        l |= c << 32;
+        l |= c << 40;
+        l |= c << 48;
+        l |= c << 56;
+        l |= c << 64;
+        return l;
     }
 
-    private static int mapByte(byte[] a, int i, char c) {
-        int r1 = mapBit(a, 8 * i, 0, c);
-        int r2 = mapBit(a, 8 * i + 1, 1, c);
-        int r3 = mapBit(a, 8 * i + 2, 2, c);
-        int r4 = mapBit(a, 8 * i + 3, 3, c);
-        int r5 = mapBit(a, 8 * i + 4, 4, c);
-        int r6 = mapBit(a, 8 * i + 5, 5, c);
-        int r7 = mapBit(a, 8 * i + 6, 6, c);
-        int r8 = mapBit(a, 8 * i + 7, 7, c);
-        return r1 | r2 | r3 | r4 | r5 | r6 | r7 | r8;
+    private static long COLON = createMask(':');
+
+    private static byte isPositive(int n) {
+        return (byte) (((~n & (~n + 1)) >> 31) & 1);
     }
 
-    private static final int WORD_SIZE = 8;
-
-    private static byte[] newArray(int length) {
-        return new byte[(length + WORD_SIZE - 1) / WORD_SIZE];
+    private static int shift(long map, int s) {
+        return 1 - ((int) (map >> s) & 0xFF);
     }
 
-    public static int parse(byte[] a) {
-        byte[] colons = newArray(a.length);
-        byte[] quotes = newArray(a.length);
-        byte[] backslashes = newArray(a.length);
-        byte[] leftBraces = newArray(a.length);
-        byte[] rightBraces = newArray(a.length);
+    private static void extractByte(byte[] r, int offset, long[] bb, int j) {
+        for (int x = 0; x < 8; x++) {
+            bb[j] |= r[offset + x] << ((7 - x) * 8);
+        }
+    }
 
-        int r = 0;
-        int delta = a.length - colons.length;
-        for (int i = 0; i < colons.length - 1 - delta; i++) {
-            colons[i] |= mapByte(a, i, ':');
-        }
-        for (int i = 0; i < colons.length - 1 - delta; i++) {
-            quotes[i] |= mapByte(a, i, '\"');
-        }
-        for (int i = 0; i < colons.length - 1 - delta; i++) {
-            backslashes[i] |= mapByte(a, i, '\\');
-        }
-        for (int i = 0; i < colons.length - 1 - delta; i++) {
-            leftBraces[i] |= mapByte(a, i, '{');
-        }
-        for (int i = 0; i < colons.length - 1 - delta; i++) {
-            rightBraces[i] |= mapByte(a, i, '}');
-        }
+    private static long buildStructuralBitmap(long x, long y) {
+        long map = x ^ y;
+        long b = 0;
 
-        return r;
+        int s1 = shift(map, 56);
+        int s2 = shift(map, 48);
+        int s3 = shift(map, 40);
+        int s4 = shift(map, 32);
+        int s5 = shift(map, 24);
+        int s6 = shift(map, 16);
+        int s7 = shift(map, 8);
+        int s8 = shift(map, 0);
+
+        byte x1 = isPositive(s1);
+        byte x2 = isPositive(s2);
+        byte x3 = isPositive(s3);
+        byte x4 = isPositive(s4);
+        byte x5 = isPositive(s5);
+        byte x6 = isPositive(s6);
+        byte x7 = isPositive(s7);
+        byte x8 = isPositive(s8);
+
+        b |= x1;
+        b |= x2 << 1;
+        b |= x3 << 2;
+        b |= x4 << 3;
+        b |= x5 << 4;
+        b |= x6 << 5;
+        b |= x7 << 6;
+        b |= x8 << 7;
+        return b;
+    }
+
+    public static long parse(byte[] r) {
+        long[] bitmap = new long[r.length / 64];
+        long sum = 0;
+        long[] bb = new long[8];
+        for (int i = 0; i < r.length - 64; i += 64) {
+            long b = 0;
+            for (int j = 0; j < bb.length; j++) {
+                bb[j] = 0;
+                extractByte(r, i, bb, j);
+            }
+
+            // TODO Apply masks to bb
+//            long r1 = buildStructuralBitmap(bb[0], COLON);
+//            long r2 = buildStructuralBitmap(bb[1], COLON) << 8;
+//            long r3 = buildStructuralBitmap(bb[2], COLON) << 16;
+//            long r4 = buildStructuralBitmap(bb[3], COLON) << 24;
+//            long r5 = buildStructuralBitmap(bb[4], COLON) << 32;
+//            long r6 = buildStructuralBitmap(b6, COLON) << 40;
+//            long r7 = buildStructuralBitmap(b7, COLON) << 48;
+//            long r8 = buildStructuralBitmap(b8, COLON) << 56;
+
+//            b = r1 | r2 | r3; //| r2 | r3 | r4 | r5 | r6 | r7 | r8;
+            for (int ii = 0; ii < 8; ii++) {
+                b |= bb[ii];
+            }
+
+            sum += b;
+        }
+        return sum;
     }
 }
--- zakim-parser/src/test/scala/mld/zakim/ParserSpec.scala
+++ zakim-parser/src/test/scala/mld/zakim/ParserSpec.scala
@@ -1,63 +1,20 @@
 //package mld.zakim
 //
-//import org.specs2.matcher.MatchResult
 //import org.specs2.mutable.Specification
-//import org.specs2.specification.core.Fragment
-//
-//import scala.io.Source
 //
 //class ParserSpec extends Specification {
-//  private def withoutWhitespace(filename: String): String =
-//    s"example-json/without-whitespace/$filename"
-//  private def withWhitespace(filename: String): String =
-//    s"example-json/with-whitespace/$filename"
 //
-//  private def loadJson(resource: String): Array[Byte] = {
-//    val s = Source.fromInputStream(
-//      getClass.getClassLoader.getResourceAsStream(resource))
-//    try s.getLines().toList.mkString("\n").getBytes("UTF-8")
-//    finally s.close()
+//  // Credit to https://stackoverflow.com/a/12310078
+//  def toBinaryString(b: Byte): String =
+//    String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0')
+//
+//  "all colons" ! {
+//    Parser.colonBitmap(Array.fill(10)(":".getBytes("UTF-8")).flatten)
+//      .map(toBinaryString).mkString(",") ==== "11111111,00000011"
 //  }
 //
-//  private def loadResult(resource: String): List[(JsonPath, Position)] =
-//    Result.read(getClass.getClassLoader.getResourceAsStream(resource))
-//
-//  private def assertPositions[T](
-//    resource: String,
-//    f: List[(JsonPath, Position)] => MatchResult[T]): MatchResult[T] =
-//    f(Parser.parse(
-//      loadJson(resource), List.empty[(JsonPath, Position)]) {
-//      case (_, path, s, e, pathToPosition) =>
-//        (path -> Position(s, e)) :: pathToPosition
-//    }.reverse)
-//
-//  "without whitespace" >> {
-//    def scenario(name: String): Fragment =
-//      name ! assertPositions(withoutWhitespace(s"$name.json"), _ ====
-//        loadResult(withoutWhitespace(s"$name.result")))
-//    scenario("empty-01")
-//    scenario("empty-02")
-//    scenario("flat-01")
-//    scenario("flat-02")
-//    scenario("flat-03")
-//    scenario("flat-04")
-//    scenario("flat-05")
-//    scenario("nested-01")
-//    scenario("nested-02")
-//    scenario("nested-03")
-//    scenario("nested-04")
-//    scenario("nested-05")
-//    scenario("nested-06")
+//  "not colons" ! {
+//    Parser.colonBitmap(Array.fill(10)("$".getBytes("UTF-8")).flatten)
+//      .map(toBinaryString).mkString(",") ==== "00000000,00000000"
 //  }
-//
-////  "with whitespace" >> {
-////    def scenario(name: String): Fragment =
-////      name ! assertPositions(withWhitespace(s"$name.json"), _ ====
-////        loadResult(withWhitespace(s"$name.result")))
-////    scenario("flat-01")
-////    scenario("flat-02")
-////    scenario("flat-03")
-//    //    scenario("nested-01")
-//    //    scenario("nested-02")
-////  }
 //}
